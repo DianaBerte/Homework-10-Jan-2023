d458783e1f0ed041c19f2ff1b98f899f04a1e20c
DianaBerte <dianaberte.go@gmail.com>
2023-01-12 16:52:17 +0100

    JS

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function deleteFunc() {
+    let bodyNode = document.getElementById('tbody');
+    let bgInfoNode = document.getElementById('bg-info');
+    bodyNode.removeChild(bgInfoNode)
+}
+
+function deleteFunc2() {
+    let bodyNode = document.getElementById('tbody');
+    let bgWarningNode = document.getElementById('bg-warning');
+    bodyNode.removeChild(bgWarningNode)
+}
